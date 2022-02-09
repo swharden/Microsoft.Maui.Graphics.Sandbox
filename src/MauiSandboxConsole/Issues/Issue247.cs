a20682c9b113d3c0d05b7b8c094dad52c333a727
Scott W Harden <swharden@gmail.com>
2022-02-08 20:16:45 -0500

    refactor sandbox

---
+++ src/MauiSandboxConsole/Issues/Issue247.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.Maui.Graphics;
+
+namespace MauiSandboxConsole.Issues;
+
+public class Issue247
+{
+    public static void Run()
+    {
+        using var bmp = new Microsoft.Maui.Graphics.Skia.SkiaBitmapExportContext(300, 200, 1.0f);
+
+        ICanvas canvas = bmp.Canvas;
+
+        IImage original = LoadImageWithSkia("../../../images/bird.jpg");
+        Console.WriteLine($"original: {original.Width}x{original.Height}");
+
+        IImage thumb = original.Downsize(200);
+        //Console.WriteLine($"thumb: {thumb.Width}x{thumb.Height}");
+        FileStream fs = new("Issue247.jpg", FileMode.Create);
+        thumb.Save(fs, ImageFormat.Jpeg);
+    }
+
+    public static IImage LoadImageWithSkia(string filePath)
+    {
+        byte[] bytes = File.ReadAllBytes(filePath);
+        MemoryStream memstr = new(bytes);
+        //var img = new Microsoft.Maui.Graphics.Skia.SkiaImageLoadingService;
+        IImage img = new Microsoft.Maui.Graphics.Skia.SkiaImage(SkiaSharp.SKBitmap.Decode(memstr));
+        return img;
+    }
+}
--- src/MauiSandboxConsole/Program.cs
+++ src/MauiSandboxConsole/Program.cs
@@ -1,31 +1,9 @@
-﻿using Microsoft.Maui.Graphics;
-using Microsoft.Maui.Graphics.Skia;
+﻿namespace MauiSandboxConsole;
 
-Random rand = new(0);
-using BitmapExportContext bmp = new SkiaBitmapExportContext(300, 200, 1.0f);
-ICanvas canvas = bmp.Canvas;
-
-// draw a test pattern in the background
-canvas.FillColor = Colors.DarkGray;
-canvas.FillRectangle(0, 0, bmp.Width, bmp.Height);
-canvas.StrokeSize = 3;
-canvas.StrokeColor = Colors.Black;
-for (int i = 0; i < bmp.Width * 2; i += 50)
-    canvas.DrawLine(0, i, i, 0);
-
-PointF pt1 = new(100, 100);
-PointF pt2 = new(200, 100);
-float radius = 75;
-
-// fill a circle with a random color using a fluent method
-canvas.FillColor = Color.FromRgba(rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255), rand.Next(100, 200));
-canvas.FillCircle(pt1, radius);
-
-// fill a circle with a random color using a constructor
-canvas.FillColor = new Color(rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255), rand.Next(100, 200));
-canvas.FillCircle(pt2, radius);
-
-string filePath = Path.GetFullPath("test.png");
-using FileStream fs = new(filePath, FileMode.Create);
-bmp.WriteToStream(fs);
-Console.WriteLine(filePath);
+public static class Program
+{
+    public static void Main()
+    {
+        Issues.Issue247.Run();
+    }
+}
